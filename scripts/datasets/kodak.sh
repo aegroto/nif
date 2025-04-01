3c9b4c27ca942d5331ff4d2889fe0ca55957d3e2
aegroto <aegroto@protonmail.com>
2025-04-01 10:52:24 +0200

    feat: reprozip support

--- scripts/datasets/kodak.sh
+++ scripts/datasets/kodak.sh
@@ -1,12 +1,16 @@
 > schedule.sh
 
+reprozip_root="results/.reprozip/nif/kodak/"
+echo "rm -r $reprozip_root" >> schedule.sh
+echo "mkdir -p $reprozip_root" >> schedule.sh
+
 for config in $(find configurations/nif/kodak/*.yaml)
 do
     config_file=$(basename $config)
     config_id="${config_file%.yaml}"
     for i in {1..24}
     do
-        log_file="logs/${config_id}_$i.txt"
-        echo "./experiment.sh $config test_images/kodak/$i.png results/nif/kodak/$config_id/$i > $log_file 2>&1" >> schedule.sh
+        results_root="results/nif/kodak/$config_id/$i"
+        echo "uv run reprozip trace --continue -d $reprozip_root bash experiment.sh $config test_images/kodak/$i.png $results_root" >> schedule.sh
     done
 done
