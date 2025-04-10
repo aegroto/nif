aa6f7e9140dd5cf3f58311916bd049a985c97cf3
aegroto <aegroto@protonmail.com>
2025-04-10 22:31:19 +0200

    feat: update ablation study scripts

--- scripts/experiments/ablations/kodak_no_modulations.sh
+++ scripts/experiments/ablations/kodak_no_modulations.sh
@@ -6,7 +6,6 @@ do
     config_id="${config_file%.yaml}"
     for i in {1..24}
     do
-        log_file="logs/${config_id}_$i.txt"
-        echo "./experiment.sh $config test_images/kodak/$i.png results/nif/kodak_no_modulations/$config_id/$i > $log_file 2>&1" >> schedule.sh
+        echo "uv run bash experiment.sh $config test_images/kodak/$i.png results/nif/kodak_no_modulations/$config_id/$i" >> schedule.sh
     done
 done
